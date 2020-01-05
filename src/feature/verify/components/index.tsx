9ecca2cd0405389d6bbe8f460b2178ce005bc2c7
Phumrapee Limpianchop <git@rayriffy.com>
2020-01-05 14:03:34 +0700

    :tada: Init verify page

---
+++ src/feature/verify/components/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Heading } from '@chakra-ui/core'
+
+const VerifyFeature: React.FC = props => {
+  return (
+    <React.Fragment>
+      <Heading>ตรวจสอบข้อมูล และยืนยันการสมัคร</Heading>
+    </React.Fragment>
+  )
+}
+
+export default VerifyFeature
--- src/pages/verify.tsx
+++ src/pages/verify.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 import { NextPage } from 'next'
 
+import VerifyComponent from '../feature/verify/components'
+
 const VerifyPage: NextPage = props => {
-  return <>OK</>
+  return <VerifyComponent />
 }
 
 export default VerifyPage
