651adda570f0ad66297fa1b832d1b5cbe0c4628b
pontakornth <pontakorn61@gmail.com>
2020-01-27 18:32:50 +0700

    Add custom font

---
+++ src/app/theme.ts
@@ -0,0 +1,10 @@
+import { theme as defaultTheme } from 'chakra-ui'
+export const theme = {
+    ...defaultTheme,
+    fonts: {
+        ...defaultTheme.fonts,
+        heading: `'Poppins', 'Prompt', Futura, -apple-system, 'Helvetica Neue', Helvetica, Arial, system-ui, sans-serif`,
+        body: `'Sarabun', -apple-system, 'Helvetica Neue', Helvetica, Arial, system-ui, sans-serif`,
+    }
+
+}
