feb71ce1b7578d759f38a49d1310386788f4f1c9
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-01-28 20:46:32 +0700

    :wrench:Fix typo and size

--- src/pages/thanks.tsx
+++ src/pages/thanks.tsx
@@ -10,8 +10,8 @@ const ThanksPage: NextPage = props => {
       <Heading size='lg' textAlign='center'>
         ขอบคุณที่สมัครเข้าร่วมค่าย Young Creator's Camp ค่ะ! 💖
       </Heading>
-      <Heading size='md' textAlign='center'>
-        ประกาศผลรับสมัครวันที่ วันที่ 7 มีนา 2563 เวลา 21.00 น.
+      <Heading size='lg' textAlign='center'>
+        ประกาศผลรับสมัครวันที่ 7 มีนา 2563 เวลา 21.00 น.
       </Heading>
       <Flex justifyContent='center' pt={6}>
         <Link href='https://ycc.in.th' _hover={{ textDecoration: 'none' }}>
