9a2e30ceaef75244356cef84aad8ea9015829a35
Phumrapee Limpianchop <git@rayriffy.com>
2020-01-28 21:59:12 +0700

    feat: fb pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import App from 'next/app'
+import Head from 'next/head'
 
 import AppShell from '../app/components'
 
@@ -15,9 +16,28 @@ class NextApp extends App<IAppProps> {
     const fullstory = process.env.FULLSTORY ? process.env.FULLSTORY : ''
 
     return (
-      <AppShell fullstory={fullstory}>
-        <Component {...pageProps} />
-      </AppShell>
+      <React.Fragment>
+        <Head>
+          {`<script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '970434653357485');
+            fbq('track', 'PageView');
+          </script>
+          <noscript><img height='1' width='1' style='display:none'
+            src='https://www.facebook.com/tr?id=970434653357485&ev=PageView&noscript=1'
+          /></noscript>`}
+        </Head>
+        <AppShell fullstory={fullstory}>
+          <Component {...pageProps} />
+        </AppShell>
+      </React.Fragment>
     )
   }
 }
