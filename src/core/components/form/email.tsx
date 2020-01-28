a212ce9682c8af418959808b34ddeb87af16a814
Phumrapee Limpianchop <git@rayriffy.com>
2020-01-28 17:39:51 +0700

    fix: types

--- src/core/components/form/email.tsx
+++ src/core/components/form/email.tsx
@@ -23,7 +23,7 @@ const FormInput: React.FC<IFormInputProps> = props => {
         id={name}
         onChange={formik.handleChange}
         value={formik.values[name]}
-        placeholder={placeholder ? placeholder : title}
+        placeholder={placeholder}
         type='email'
       />
       <FormErrorMessage>{formik.errors[name]}</FormErrorMessage>
