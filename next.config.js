f50e8f95ac544d7c51a6203eaeaff632a872701f
Phumrapee Limpianchop <git@rayriffy.com>
2020-02-11 20:26:55 +0700

    fix: tabs to space

--- next.config.js
+++ next.config.js
@@ -3,43 +3,43 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 const withPreact = (nextConfig = {}) => {
-	return Object.assign({}, nextConfig, {
-		webpack(config, options) {
-			if (!options.defaultLoaders) {
-				throw new Error(
-					"This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade"
-				)
-			}
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (!options.defaultLoaders) {
+        throw new Error(
+          "This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade"
+        )
+      }
 
-			if (options.isServer) {
-				config.externals = ["react", "react-dom", ...config.externals]
-			}
+      if (options.isServer) {
+        config.externals = ["react", "react-dom", ...config.externals]
+      }
 
-			config.resolve.alias = Object.assign({}, config.resolve.alias, {
-				react: "preact/compat",
-				react$: "preact/compat",
-				"react-dom": "preact/compat",
-				"react-dom$": "preact/compat"
-			})
+      config.resolve.alias = Object.assign({}, config.resolve.alias, {
+        react: "preact/compat",
+        react$: "preact/compat",
+        "react-dom": "preact/compat",
+        "react-dom$": "preact/compat"
+      })
 
-			if (typeof nextConfig.webpack === "function") {
-				return nextConfig.webpack(config, options)
-			}
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options)
+      }
 
-			return config
-		}
-	})
+      return config
+    }
+  })
 }
 
 const withSass = require('@zeit/next-sass')
 
 module.exports = withSass(
-	withPreact(
-		withBundleAnalyzer({
-			env: {
-				FULLSTORY: process.env.FULLSTORY,
-			},
-			exportTrailingSlash: true,
-		})
-	)
+  withPreact(
+    withBundleAnalyzer({
+      env: {
+        FULLSTORY: process.env.FULLSTORY,
+      },
+      exportTrailingSlash: true,
+    })
+  )
 )
